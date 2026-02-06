2acb7ad02eb237cac5ab2723946126910b664c6a
Amar Hakim <amarhakimhamzah@gmail.com>
2026-02-06 11:10:49 +0700

    edit-product: membuat fitur edit product dengan menambahkan kode di file ProductController.java, ProductRepository.java, ProductService.java, dan ProductServiceImpl.java. Serta menambahkan file EditProduct.html

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -33,6 +33,20 @@ public class ProductController {
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
+
+    @GetMapping("/edit/{id}")
+    public String editProductPage(@PathVariable String id, Model model) {
+        Product product = service.findById(id);
+        model.addAttribute("product", product);
+        return "EditProduct";
+    }
+
+    @PostMapping("/edit")
+    public String editProductPost(@ModelAttribute Product product) {
+        service.update(product);
+        return "redirect:/product/list";
+    }
+
 }
